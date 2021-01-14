9280f5b529500c5c5ce83c94628bad0fd11c2e21
Kushagra Garg <kushagragarg1291@gmail.com>
2021-01-14 15:12:32 +0530

    database configured

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +94,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
