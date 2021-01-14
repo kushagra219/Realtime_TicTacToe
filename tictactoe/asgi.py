a1eb18f331c93323b73169fd0f5ec63e838fe09c
Kushagra Garg <kushagragarg1291@gmail.com>
2021-01-14 14:33:44 +0530

    deployment error

--- tictactoe/asgi.py
+++ tictactoe/asgi.py
@@ -6,7 +6,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from app import consumers
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tictactoe.settings")
 
 ws_urlpatterns = [
     path('ws/game/<room_code>/', consumers.GameRoom.as_asgi())
