85e20d3f2be28f40d3c9c55e2e65ae063f8e674a
Kushagra Garg <kushagragarg1291@gmail.com>
2021-01-14 15:02:20 +0530

    deploy success, static files configure

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tictactoe.urls'
@@ -129,8 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     BASE_DIR / "static",
     '/var/www/static/',
-]
+)
+
+
