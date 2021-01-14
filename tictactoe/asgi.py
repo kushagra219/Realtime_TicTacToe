9726a13df2f259bb3318f4b0382cac9bd289bdb5
Kushagra Garg <kushagragarg1291@gmail.com>
2021-01-14 14:50:11 +0530

    heroku deployment try 3

--- tictactoe/asgi.py
+++ tictactoe/asgi.py
@@ -1,12 +1,13 @@
 import os
-
+import django
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from app import consumers
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tictactoe.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
+django.setup()
 
 ws_urlpatterns = [
     path('ws/game/<room_code>/', consumers.GameRoom.as_asgi())
